dfaf55785f44eacd0b21603dc560c9a2a07f12ab
Jacckol <ajp.mendoza@yavirac.edu.ec>
2023-05-31 22:39:12 -0500

    nuevos cambios

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { AdminModel, CreateProvidersModelDto, UpdateProvidersModelDto } from '../entities/Admin.model';
+import { map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminService {
+export class ProvidersService {
 
-  constructor() { }
+  readonly API_URL = 'https://api.escuelajs.co/api/v1/products';
+  constructor(private httpClient: HttpClient) { }
+  
+  getAll(): Observable<AdminModel[]> {
+    const url = `${this.API_URL}`;
+    return this.httpClient.get<AdminModel[]>(url);
+  }
+
+  getOne(id: AdminModel['id_Admin']): Observable<AdminModel> {
+    const url = `${this.API_URL}/${id}`;
+    return this.httpClient.get<AdminModel>(url);
+  }
+
+  store(providers: CreateProvidersModelDto): Observable<AdminModel> {
+    const url = `${this.API_URL}`;
+    return this.httpClient.post<AdminModel>(url, providers  );
+  }
+
+  update(id: AdminModel['id_Admin'],providers: UpdateProvidersModelDto): Observable<AdminModel> {
+    const url = `${this.API_URL}/${id}`;
+    return this.httpClient.put<AdminModel>(url, providers);
+  }
+
+  destroy(id: AdminModel['id_Admin']):Observable<boolean>  {
+    const url = `${this.API_URL}/${id}`;
+    return this.httpClient.delete<any>(url).pipe(map((response: { rta: boolean }) => { return response.rta })
+    );
+  }
 }
